e82adc35c3b0ac5511170a712b4555c67ad1f6cf
gtalmno05 <vaccaroguido2005@gmail.com>
2021-11-18 20:45:23 +0100

    Create pag 110 n7 scomporre un numero.cpp

---
+++ pag 110 n7 scomporre un numero.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+ 
+ int main()
+{
+
+	//variabili
+	int num;
+	int a;
+	int b;
+	int c;
+	int d;
+	int scomp;
+	
+	//input dati
+	cout<<" inserisci il numero: ";
+	cin>>num;
+	cout<<"\n";
+
+	//risposta
+	if (num<=9999){
+		if(num>=1000){ 
+			
+		}
+	}
+	
+
+	system("pause");
+	return 0;
+ } 
