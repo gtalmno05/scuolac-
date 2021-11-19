12b91d2f1099667f92dcf6843ee1620066e7510c
gtalmno05 <79110990+gtalmno05@users.noreply.github.com>
2021-11-19 13:09:10 +0100

    Add files via upload

---
+++ Pag. 110 num. 7.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//variabili
+int numero;
+
+//input dati
+cout<<"inserisci il numero da scomporre: ";
+scanf("%d",&numero);
+
+//risposta
+if (numero<=9999){
+	if (numero>=1000){
+				while (numero>0){
+				printf("%d\n",numero%10);
+				numero = numero/10;
+}
+	}
+	else cout<<"Inserire un numero compreso tra 1000 e 9999 :\n";
+	scanf("%d",&numero);{
+	if (numero<=9999){
+	if (numero>=1000){
+				while (numero>0){
+				printf("%d\n",numero%10);
+				numero = numero/10;
+}
+	}
+	}
+}
+}
+return 0;
+}
