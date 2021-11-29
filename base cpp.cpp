c4851824106f64ca182d2375b35fa88b9d876ce4
gtalmno05 <vaccaroguido2005@gmail.com>
2021-11-29 09:59:53 +0100

    Create base cpp.cpp

---
+++ base cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"\t"<<"\t"<<"\t"<<"\t"<<"\t"<<"\t"<<"questo programma "<<endl;
+	
+	//variabili
+	
+	//input dati
+	
+	//calcolo
+	
+	system("pause");
+	return 0;
+}
