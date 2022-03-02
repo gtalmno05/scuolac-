919ea1c01582d4e2c312abf45b981d5a97c8ca58
gtalmno05 <vaccaroguido2005@gmail.com>
2022-03-02 18:03:56 +0100

    Create primi 100 numeri naturali.cpp

---
+++ primi 100 numeri naturali.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+ 
+ int main()
+{
+	//i primi 100 numeri naturali
+	//variabili
+	int i;
+	int n;
+
+	//operazioni&risposta
+	i=0;
+	n=0;
+	for(i=0;i<100;i++)
+	{
+		n+=1;
+		cout<<n<<endl;
+	}
+
+	system("pause");
+	return 0;
+ } 
