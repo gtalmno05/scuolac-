c5e7dc37a7afb903ac700cd3ce2deb23a5f3ec0e
gtalmno05 <79110990+gtalmno05@users.noreply.github.com>
2022-04-11 12:24:57 +0200

    Update pag 250 n10.cpp

--- pag 250 n10.cpp
+++ pag 250 n10.cpp
@@ -6,21 +6,27 @@ using namespace std;
 
 //variabili globali
 int nv;
-int v[nv];
+int v[10];
 
-
+int ordinearray(int sceltaord);
+int arraycasuale();
+int sceltaordine(int &sceltaord);
+int sceltatipologia(int &sceltatip);
+int numeritot(int &nv);
 int main()
 {
 	int n;
 	int sceltatip;
 	int sceltaord;
+	int v[nv];
+	
 	sceltatipologia(sceltatip);
 	
 	switch(sceltatip)
 	{
 		case 0:
 			
-			numeritot(nv)
+			numeritot(nv);
 			arraycasuale();
 			sceltaordine(sceltaord);
 			
@@ -28,10 +34,10 @@ int main()
 			{
 				case 0:
 					
-					
+				ordinearray(sceltaord);	
 					
 				break;
-					
+		
 				case 1:
 					
 					
@@ -66,10 +72,11 @@ int main()
 	}
 }
 
-numeritot(int &nv)
+int numeritot(int &nv)
 {
 	
 	cout<<"inserisci il totale dei numeri che vuoi: ";
+	cout<<"\n";
 	cin>>nv;
 	cout<<"\n";
 	
@@ -78,8 +85,10 @@ numeritot(int &nv)
 int sceltatipologia(int &sceltatip)
 {
 	cout<<"Inserisci come vuoi riempire il vettore :";
+	cout<<"\n";
 	cout<<"0:casuale; 1:manuale";
-	cin>>scelta;
+	cout<<"\n";
+	cin>>sceltatip;
 	cout<<"\n";
 	
 }
@@ -87,8 +96,50 @@ int sceltaordine(int &sceltaord)
 {
 	
 	cout<<"Inserisci come vuoi visualizzare il vettore :";
+	cout<<"\n";
 	cout<<"0:da destra verso sinistra; 1:da sinistra verso destra";
+	cout<<"\n";
 	cin>>sceltaord;
 	cout<<"\n";
+	
+}
+
+int arraycasuale()
+{
+	int i;
+	
+	for(i=0;i<nv;i++)
+	{
+		
+		v[i]=(rand()%nv);
+		
+	}
+	
+}
+
+int ordinearray(int sceltaord)
+{
+	int i;
+	switch(sceltaord)
+	{
+		
+		case 0:
+			
+			for(i=0;i>nv;i--)
+			{
+				
+				cout<<v[i]<<endl;
+				
+			}
+			
+		break;
+		
+		case 1:
+			
+			
+			
+		break;
+		
+	}
 	
 }
